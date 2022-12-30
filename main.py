575c57c313fba007043ad96ab4810ce96ece12ec
Stéphane Bouchard <sbouchard25@gmail.com>
2022-12-30 11:02:52 -0500

    wip

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from machine import Pin, UART, I2C
 #Import utime library to implement delay
 import utime, time
 import network
+import sys
+
+#from umqtt.simple import MQTTClient
 
 #________________________________________________________
 from ssd1306 import SSD1306_I2C
@@ -20,24 +23,22 @@ onboard_led.on()
 heater_led = Pin(15, Pin.OUT)
 heater_led.off()
 
+# def beanaproblem(string):
+#     refresh(ssd, True)  # Clear any prior image
+#     relaypin = Pin(15, mode = Pin.OUT, value =0 )
+#     utime.sleep(2)
 
-##########################################################
-#Oled I2C connection
-#i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
-#oled = SSD1306_I2C(128, 64, i2c)
-
-#oled.fill(0)
-#oled.text("No Data", 0, 0)
-#oled.show()
-#utime.sleep(1)
-
-ds_pin = machine.Pin(18)
- 
+# Look for thermometer (add OLED complaint if one can't be seen)
+ds_pin = Pin(18)
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
-
 roms = ds_sensor.scan()
+print('Thermometer: ', roms)
+if roms=='':
+    print('No Thermometer. STOP')
+    # beanaproblem('No Therm.')
+    utime.sleep(60)
+    sys.exit()
 
-print('Found DS devices: ', roms)
 
 WIDTH  = 128                                            # oled display width
 HEIGHT = 64                                             # oled display height
@@ -62,8 +63,10 @@ print("I2C Configuration: "+str(i2c))                   # Display I2C config
 
 oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)                  # Init oled display
 
+oled.rotate(True)
+
 oled.fill(0)
-oled.rotate(False)
+#oled.rotate(False)
 oled.text("Battery Heater", 0, 0)
 oled.show()
 
@@ -85,56 +88,83 @@ ip = wlan.ifconfig()[0]
 print(f'IP Address: {ip}')
 
 oled.fill(0)
-oled.text("IP Address", 0, 0)
-oled.text(ip, 0, 16)
-#oled.rotate(True)
+oled.text("IP: "+ip, 0, 0)
+#oled.text(ip, 0, 16)
 oled.show()
 
-utime.sleep(1)
+# utime.sleep(1)
+# oled.poweroff()
+# oled.fill(0)
+# oled.text("IP: "+ip, 0, 0)
+# #oled.text("IP Address", 0, 0)
+# #oled.text(ip, 0, 16)
+# #oled.rotate(False)
+# oled.show()
 
-oled.poweroff()
+# utime.sleep(2)
 
-oled.fill(0)
-oled.text("IP Address", 0, 0)
-oled.text(ip, 0, 16)
-#oled.rotate(False)
-oled.show()
-
-utime.sleep(2)
-
-oled.poweron()
-
+# oled.poweron()
 
 push_button = Pin(22, Pin.IN)  # 22 number pin is input
 
-solid_state_relay = Pin(28, Pin.OUT)
-solid_state_relay.value(0)
+relaypin = Pin(28, Pin.OUT)
+relaypin.value(0)
+
+
+# Main Logic
+pin = 0
+counter = 54.5
+integral = 0
+lastupdate = utime.time()  
+#refresh(ssd, True)  # Initialise and clear display.
+
+lasterror = 0
+# The Tweakable values that will help tune for our use case. TODO: Make accessible via menu on OLED
+checkin = 5
+# Stolen From Reddit: In terms of steering a ship:
+# Kp is steering harder the further off course you are,
+# Ki is steering into the wind to counteract a drift
+# Kd is slowing the turn as you approach your course
+Kp = 20.   # Proportional term - Basic steering (This is the first parameter you should tune for a particular setup)
+Ki = .01   # Integral term - Compensate for heat loss by vessel
+Kd = 150.  # Derivative term - to prevent overshoot due to inertia - if it is zooming towards setpoint this
+           # will cancel out the proportional term due to the large negative gradient
+output = 0
+offstate = False
+boil = False  # The override flag that will just get to a boil as quick as possible. (Assumes water at sea level, which is ok for now)
+# Heating loop - Default behaviour
+powerup = True
 
 try:
     while True:
         logic_state = push_button.value()
 
         ds_sensor.convert_temp()
-        time.sleep_ms(750)
-        
-        for rom in roms:
-            print(rom)
-            print(ds_sensor.read_temp(rom))
+        temp = ds_sensor.read_temp(roms[0])
+        #time.sleep_ms(750)
+
+        now = utime.time()
+        dt = now - lastupdate
+
+        # for rom in roms:
+        #     #print(rom)
+        #     print(ds_sensor.read_temp(rom))
 
         # Clear the oled display in case it has junk on it.
-        oled.fill(0)       
-        
+        oled.fill(0)
         # Add some text
-        oled.text("Temperature",20,8)
-        oled.text(str(round(ds_sensor.read_temp(rom),1)),36,24)
+        oled.text("IP: "+ip, 0, 0)
+        oled.text("Temperature",20,16)
+        print("temp: "+str(temp))
+        oled.text(str(round(temp,1)),36,24)
         oled.text("*C",76,24)
         oled.text("Heater",40,40)
         if logic_state == True:
             heater_led.on()
             oled.text("ON",56, 56)
-            solid_state_relay.value(1)
+            relaypin.value(1)
         else:
-            solid_state_relay.value(0)
+            relaypin.value(0)
             oled.text("OFF",52, 56)
             heater_led.off()
 
@@ -145,8 +175,8 @@ try:
         
 finally:    
     # Clear the oled display in case it has junk on it.
-    solid_state_relay.value(0)
+    relaypin.value(0)
     oled.fill(0)
     oled.show()
-    onboard_led.off()
     heater_led.off()
+    onboard_led.off()
